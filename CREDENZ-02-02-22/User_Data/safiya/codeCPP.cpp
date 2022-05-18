ce10ce3bac644c9ed774d49bd5f87600132b9813
sangeetasingh17 <sdsinghh17@gmail.com>
2022-05-18 11:24:29 +0530

    final

---
+++ CREDENZ-02-02-22/User_Data/safiya/codeCPP.cpp
@@ -0,0 +1,40 @@
+#include "/home/jas/Desktop/ClashRCRound2Backend-final-clash/CREDENZ-02-02-22/sandbox/filter/filter.h"
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+put_filter();
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto timenow =
+      chrono::system_clock::to_time_t(chrono::system_clock::now());
+  
+    long long int t;
+    cin>>t;
+    long long int n;
+    while(t--)
+    {
+        cin>>n;
+        long long int a[n];
+        long long int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            sum+=a[i];
+        }
+        sort(a,a+n);
+        reverse(a, a + n);
+        for(int i=0;i<n;i++)
+        {
+            if((i + 1) % 5 == 0)
+            {
+                sum-=a[i];
+            }
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+    cout << ctime(&timenow) << endl;
+}
